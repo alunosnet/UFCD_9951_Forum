2964647174f86c12306f663f632430799cb05aa7
alunosNET <alunosnet@gmail.com>
2023-01-05 23:15:51 +0000

    Listar e atualizar utilizadores

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect
 from basedados import *
 
 tabela_utilizador="create table if not exists utilizador(id integer primary key autoincrement, nome text, email text, palavra_passe text, estado integer)"
@@ -36,6 +36,37 @@ def utilizador_registar():
         parametros=(nome,email,palavra_passe,1)
         ExecutarSQL(sql,parametros)
         #redirecionar
-        
+        return redirect('/utilizador/listar')
+
+@app.route('/utilizador/listar')
+def utilizador_listar():
+    #consultar a bd para obter a lista de utilizadores
+    sql = "SELECT * FROM Utilizador"
+    dados = DevolverSQL(sql)
+    return render_template('utilizador/listar.html',registos=dados)
+
+@app.route('/utilizador/apagar',methods=["POST"])
+def utilizador_apagar():
+    #recolher o id do utilizador a apagar
+    id = request.form.get("id")
+    #consulta à bd para ter os dados do utilizador
+    sql = "SELECT * FROM Utilizador WHERE id=?"
+    parametros=(id,)
+    dados = DevolverSQL(sql,parametros)
+    #mostrar a página para confirmar o apagar
+    return render_template("utilizador/apagar.html",registo=dados[0])
+
+@app.route("/utilizador/apagar_confirmado",methods=["POST"])
+def utilizador_apagar_confirmado():
+    #recolher o id do utilizador a apagar
+    id = request.form.get("id")
+    #apagar da bd
+    sql = "DELETE FROM Utilizador WHERE id=?"
+    parametros=(id,)
+    ExecutarSQL(sql,parametros)
+    #redirecionar para utilizador/listar
+    return redirect("/utilizador/listar")
+
+#rota para editar
 
 app.run()
